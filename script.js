43f4a7a12a80ff7f61677bdd3c031b3247cbb9a6
Kubrianity <kubraaozerenn@gmail.com>
2022-07-05 16:50:07 +0300

    Add the input value as a property of item object

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+    e.preventDefault(); // stop page from reloading
+    const text = (this.querySelector('[name=item]')).value; // "this" refers to the actual form
+    const item = {
+        text,
+        done: false
+    }
+
+}
+addItems.addEventListener("submit", addItem)
